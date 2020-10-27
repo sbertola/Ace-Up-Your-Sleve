fb825cd681c40e25da8588bc43eb24a499a7da1c
Sebastian Bertola <sbertola@uoguelph.ca>
2020-10-26 20:58:38 -0400

    Added Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 1;
+    [SerializeField] private float jumpForce = 1;
+    [SerializeField] private float jumpDelay = 0.001f;
+
+    private Rigidbody2D rigidbody2D;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
+
+        if(Input.GetButtonDown("Jump") && Mathf.Abs(rigidbody2D.velocity.y) < jumpDelay)
+        {
+            rigidbody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
